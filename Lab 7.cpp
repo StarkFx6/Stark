30350611bb24c4c939f4793827fd94188ca609b5
StarkFx6 <nik57kondr@gmail.com>
2023-10-31 15:18:50 +0200

    Add Lab7

---
+++ Lab 7.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include <iostream>
+
+using  namespace std;
+
+int main()
+{
+    
+	
+	int y, a = 3;
+	cout << "Enter y = ";
+	cin >> y;
+	float rez;
+	int mno = 0;
+	for (int n = 1; n <= 3; n++)
+	{
+		mno = mno * pow(n, 2);
+	}
+	rez = pow(y, 3)+ mno;  
+	cout << "Z = " << rez << endl;
+}
+
+
